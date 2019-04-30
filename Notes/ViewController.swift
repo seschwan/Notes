ec392684e4bcb76650e43f47627e373a06a92f41
Corey Sessions <corey.iosdev@yahoo.com>
2019-04-29 19:41:13 -0600

    Start Project Notes

---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Corey Sessions on 4/29/19.
+//  Copyright © 2019 Corey Sessions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
